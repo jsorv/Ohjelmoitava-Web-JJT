c900007b169c4b3de09c5e365dee2cfb86c8d42c
jsorv <jenna.sorvoja@gmail.com>
2026-02-25 20:59:26 +0200

    report.py and models.py edits

--- API/src/weatherradar/resources/report.py
+++ API/src/weatherradar/resources/report.py
@@ -1,27 +1,61 @@
+from flask import Response, request, url_for
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from jsonschema import validate
+
 from weatherradar.models import WeatherReport
+from weatherradar import db
 
 class WeatherReports(Resource):
 
     #todo
     def get(self):
-        pass
+        response_data = []
+        reports = WeatherReport.query.all()
+        for report in reports:
+            response_data.append(report.serialize())
+        return response_data
     
     #todo
     def post(self):
-        pass
-
-
+        if not request.json:
+            return {"error": "Invalid JSON"}, 415
+        try:
+            validate(request.json, WeatherReport.json_schema())
+        except KeyError as e:
+            return {"error": f"Missing field: {str(e)}"}, 400
+        
+        weatherReport = WeatherReport()
+        weatherReport.deserialize(request.json)
+        try:
+            db.session.add(weatherReport)
+            db.session.commit()
+        except IntegrityError:
+            return {"error": "Weather report for this location and timestamp already exists."}, 409
+        return {"message": "Weather report created successfully."}, 201
+        
 class WeatherReportItem(Resource):
 
-    #todo
     def get(self, report):
         return report.serialize()
 
-    #todo
     def put(self, report):
-        pass
+        if not request.json:
+            return {"error": "Invalid JSON"}, 415
+        try:
+            validate(request.json, WeatherReport.json_schema())
+        except KeyError as e:
+            return {"error": f"Missing field: {str(e)}"}, 400
+        
+        report.deserialize(request.json)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return {"error": "Weather report for this location and timestamp already exists."}, 409
+        return {"message": "Weather report updated successfully."}, 200
     
-    #todo
     def delete(self, report):
-        pass
+        db.session.delete(report)
+        db.session.commit()
+
+        return {"message": "Weather report deleted successfully."}, 200
